3d73ac258a27907ea480a499adbe670f56d1a377
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-08 16:13:57 +0200

    eomdified tests for storage engines

--- models/__init__.py
+++ models/__init__.py
@@ -9,9 +9,11 @@ from os import getenv
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
+    print("This is db engine")
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    print("This is file storage engine")
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
