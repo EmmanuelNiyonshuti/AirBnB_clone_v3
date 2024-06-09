88e99a24017365798ac451f06b737cb66a483859
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-06-09 09:50:30 +0200

    stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ This module defines routes related to the status of the API.
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
@@ -19,3 +20,19 @@ def status():
         JSON response: A JSON object with a key "status" and value "OK".
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """
+    Retrieves The number of each object by type.
+    """
+    obj_dict = {}
+    for obj in storage.all().values():
+        class_name = obj.__class__.__name__
+        if class_name.endswith('y'):
+            key = class_name[:-1].lower() + 'ies'
+        else:
+            key = class_name.lower() + 's'
+        obj_dict[key] = storage.count(class_name)
+    return jsonify(obj_dict)
